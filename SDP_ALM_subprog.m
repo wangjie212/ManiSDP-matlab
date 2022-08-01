1c20627ca7ea535b8a3b79affe63b60e3f0cd029
huliangbing <huliangbing2000@163.com>
2022-08-01 23:01:57 +0800

    Using elliptopefactory manifold

--- SDP_ALM_subprog.m
+++ SDP_ALM_subprog.m
@@ -1,8 +1,8 @@
 function  [Y, fval, info] = SDP_ALM_subprog(A, At, b, C, c, n, p, sigma, yk, Y0)
     % Pick the manifold of n-by-p matrices with unit norm rows.
     % manifold = obliquefactory(p, n, true);
-    % manifold = elliptopefactory(n, p);
-    manifold = symfixedrankYYfactory(n, p);
+    manifold = elliptopefactory(n, p);
+    %manifold = symfixedrankYYfactory(n, p);
     problem.M = manifold;
     
     % Define the cost function to be /minimized/.
@@ -42,8 +42,8 @@ function  [Y, fval, info] = SDP_ALM_subprog(A, At, b, C, c, n, p, sigma, yk, Y0)
     opts = struct();
     opts.verbosity = 2;      % Set to 0 for no output, 2 for normal output
     opts.maxinner = 30;     % maximum Hessian calls per iteration
-    opts.tolgradnorm = 1e-3; % tolerance on gradient norm
-    opts.maxiter = 500;
+    opts.tolgradnorm = 5e-3; % tolerance on gradient norm
+    opts.maxiter = 100;
     [Y, fval, info] = trustregions(problem, Y0, opts);
 
 end
